e3a86af23fc01d98d1a3ac598bc55ff72e25ee2d
superPuero <ar73mch1k@gmail.com>
2026-05-23 13:10:42 +0200

    changes

---
+++ mluvify_be/app/criteria/lexical_richness.py
@@ -0,0 +1,35 @@
+from lexicalrichness import LexicalRichness
+from app.core.context import CriteriaContext, MessageEntry
+from pydantic import BaseModel
+
+class LexicalRichnessData(BaseModel):
+    data: dict[str, float] 
+
+def get_lexical_metrics(criteria_context: CriteriaContext, message_entry: MessageEntry) -> LexicalRichnessData:
+    if not message_entry.parts_and_lemmas.lemmas:
+        return {"ttr": 0.0, "mattr": 0.0, "mtld": 0.0, "hdd": 0.0}
+
+    lex = LexicalRichness(message_entry.parts_and_lemmas.lemmas, preprocessor=None, tokenizer=None)
+
+    ttr = lex.ttr
+
+    window_size = 25 if len(message_entry.parts_and_lemmas.lemmas) >= 25 else len(message_entry.parts_and_lemmas.lemmas)
+    mattr = lex.mattr(window_size=window_size)
+
+    try:
+        mtld = lex.mtld(threshold=0.72)
+    except Exception:
+        mtld = 0.0
+
+    try:
+        draws = 42 if len(message_entry.parts_and_lemmas.lemmas) >= 42 else len(message_entry.parts_and_lemmas.lemmas)
+        hdd = lex.hdd(draws=draws)
+    except Exception:
+        hdd = 0.0
+
+    return LexicalRichness({
+        "ttr": ttr,
+        "mattr": mattr,
+        "mtld": mtld,
+        "hdd": hdd
+    })
