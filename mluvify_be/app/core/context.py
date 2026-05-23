e4cca3cfac72c758b594c3cad4db0a777dadd405
Woid <62186908+woidptr@users.noreply.github.com>
2026-05-23 10:06:28 +0200

    fix

--- mluvify_be/app/core/context.py
+++ mluvify_be/app/core/context.py
@@ -1,25 +1,10 @@
 from typing import Annotated
 from fastapi import Depends
-from ollama import AsyncClient
-from faster_whisper import WhisperModel
-
-from app.core.ollama import OllamaDep
-from app.core.whisper import WhisperDep
 
 
 class CriteriaContext:
-    def __init__(self, ollama: AsyncClient, whisper: WhisperModel) -> None:
-        self.ollama: AsyncClient = ollama
-        self.whisper: WhisperModel = WhisperModel
-
+    def __init__(self) -> None:
         self.criterias: dict[str, list[float]] = {}
-
-
-async def get_criteria_context(
-    ollama: OllamaDep,
-    whisper: WhisperDep,
-):
-    pass
 
 
 # CriteriaContextDep = Annotated[CriteriaContext, Depends()]
