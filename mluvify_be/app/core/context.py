7d82802fd4c0e79d5a1f3af482f01531ebd1624f
superPuero <ar73mch1k@gmail.com>
2026-05-23 10:08:56 +0200

    criteria context changes

--- mluvify_be/app/core/context.py
+++ mluvify_be/app/core/context.py
@@ -1,10 +1,11 @@
 from typing import Annotated
 from fastapi import Depends
 
-
 class CriteriaContext:
     def __init__(self) -> None:
         self.criterias: dict[str, list[float]] = {}
+        self.message:  list[str] = {}    
 
+criteria_context: CriteriaContext = CriteriaContext();
 
-# CriteriaContextDep = Annotated[CriteriaContext, Depends()]
+CriteriaContextDep = Annotated[CriteriaContext, Depends(criteria_context)]
